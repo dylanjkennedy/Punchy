0e718352f1dfa109f1ad441c0cd8ad275819bf50
emilyblackman <emilyblackman2019@u.northwestern.edu>
2018-10-13 21:44:30 -0500

    set up nice new architecture and moved camera controls into new scripts

---
+++ Assets/Personal/Scripts/PlayerMover.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson; //FirstPersonController made this the namespace
+
+
+public class PlayerMover : MonoBehaviour
+{
+
+    public PlayerState currentState;
+    [SerializeField] private MouseLook _mouseLook;
+    public MouseLook mouseLook
+    {
+        get { return _mouseLook; }
+    }
+
+    private Camera m_Camera;
+
+    // Use this for initialization
+    void Start () {
+        m_Camera = Camera.main;
+        mouseLook.Init(transform, m_Camera.transform);
+        currentState = new PlayerState(this);
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+	    PlayerState newState = currentState.FixedUpdate();
+	    if (newState != null)
+	    {
+	        currentState.Exit();
+	        newState.Enter();
+	        currentState = newState;
+	    }
+	}
+
+    void Update()
+    {
+        currentState.Update();
+    }
+
+}
---
+++ Assets/Personal/Scripts/PlayerState.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class PlayerState
+{
+    private PlayerMover playermover;
+    private MouseLook mouseLook;
+    private Camera cam;
+    public PlayerState(PlayerMover pm)
+    {
+        playermover = pm;
+        mouseLook = playermover.mouseLook;
+        cam = Camera.main;
+    }
+    public virtual PlayerState FixedUpdate()
+    {
+        MouseLookFixedUpdate();
+        return null;
+    }
+
+    public virtual void Update()
+    {
+        MouseLookUpdate();
+    }
+    public virtual void Enter()
+    {
+
+    }
+
+    public virtual void Exit()
+    {
+
+    }
+    public virtual void MouseLookFixedUpdate()
+    {
+        Debug.Log("MouseLookFixedUpdate");
+        mouseLook.UpdateCursorLock();
+    }
+    public virtual void MouseLookUpdate()
+    {
+        Debug.Log("MouseLookUpdate");
+        RotateView();
+    }
+
+    private void RotateView()
+    {
+        mouseLook.LookRotation(playermover.transform, cam.transform);
+    }
+
+}
