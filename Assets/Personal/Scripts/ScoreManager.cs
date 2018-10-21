a2041fc5d4ff539a2860a118ddbbebc596bbf631
LueptowK <kylelueptow@gmail.com>
2018-10-21 02:36:20 -0500

    Simple combo system and minor performance increase
    Simple combo system - killing an enemy within 2 ingame seconds of killing another grants +1 combo, which is a score multiplier. Updated layer collision matrix for what should be a small performance boost.

--- Assets/Personal/Scripts/ScoreManager.cs
+++ Assets/Personal/Scripts/ScoreManager.cs
@@ -5,20 +5,34 @@ using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour {
     private int score;
+    [SerializeField] float comboTimeout;
     [SerializeField] Text scoreText;
+    [SerializeField] Text comboText;
+    float timer;
+    int combo;
 	// Use this for initialization
 	void Start () {
         score = 0;
+        timer = 0;
+        combo = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        timer += Time.deltaTime;
+        if (timer >= comboTimeout)
+        {
+            combo = 0;
+            comboText.text = combo.ToString();
+        }
 	}
 
     public void changeScore(int change)
     {
-        score += change;
+        combo++;
+        timer = 0;
+        score += change*combo;
         scoreText.text = score.ToString();
+        comboText.text = combo.ToString();
     }
 }
