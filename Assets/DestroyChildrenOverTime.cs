7c24fa48869214fbfc4246e740b0d8cd6a5473e5
LueptowK <kylelueptow@gmail.com>
2019-02-12 20:19:25 -0600

    Minor script optimization
    Unfortunately this creates a new script I hoped would be redundant, but actually makes a tangible performance difference. On my desktop I found that the DestroyOverTime script actually was taking up to 11ms total between all instances at one point (more than half a frame), which is massive (CylinderEnemyController.Update() takes on average about 0.004ms per cylinder for comparison) and likely much worse on slower machines. I believe this new solution is slightly worse than a hundred times faster.

---
+++ Assets/DestroyChildrenOverTime.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class DestroyChildrenOverTime : MonoBehaviour
+{
+    [SerializeField] float timeToDestroy;
+    MeshRenderer[] childrenMeshRenderers;
+    float timeAlive;
+    [SerializeField] float fadeTime;
+    Material material;
+    Color fadeColor;
+    float currentAlpha;
+    float lerp;
+    float timeFading;
+    // Use this for initialization
+    void Start()
+    {
+        childrenMeshRenderers = GetComponentsInChildren<MeshRenderer>();
+        timeAlive = 0;
+        if (fadeTime > 0)
+        {
+            material = childrenMeshRenderers[0].material;
+            fadeColor = material.color;
+        }
+        timeFading = 0f;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        timeAlive += Time.fixedDeltaTime;
+        if (timeAlive >= timeToDestroy)
+        {
+            Destroy(this.gameObject);
+        }
+
+        if (timeToDestroy - timeAlive <= fadeTime && fadeTime > 0)
+        {
+            timeFading += Time.deltaTime;
+            currentAlpha = 1 - (timeFading / fadeTime);
+            fadeColor.a = currentAlpha;
+            foreach (MeshRenderer childMeshRenderer in childrenMeshRenderers)
+            {
+                childMeshRenderer.material.color = fadeColor;
+            }
+            
+            
+        }
+    }
+}
