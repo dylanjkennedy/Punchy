a37961cd6a566bade587afdba072797d7e1c7c8f
emilyblackman <emilyblackman2019@u.northwestern.edu>
2019-02-14 17:41:04 -0600

    Waves are working. Needs more work and tweaking but basic functionality is there

--- Assets/Personal/Scripts/Enemy Scripts/WaveManager.cs
+++ Assets/Personal/Scripts/Enemy Scripts/WaveManager.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class WaveManager : MonoBehaviour
 {
-    bool inWave = true;
-    float timeSinceWaveStart = 0f;
+    bool inWave = false;
     float timeSinceLastWaveEnd = 0f;
     int totalEnemiesSpawned = 0;
-    [SerializeField] int totalEnemiesInWave = 5;
+    int waveCount = 0;
+    [SerializeField] int totalEnemiesInWave = 20;
+    [SerializeField] int timeBetweenWaves = 7; // seconds
     [SerializeField] SpawnManager spawnManager;
+    [SerializeField] DifficultyManager difficultyManager;
+    
+
 
     // Start is called before the first frame update
     void Start()
@@ -23,51 +27,65 @@ public class WaveManager : MonoBehaviour
         // logic for checking the time since the wave ended and setting inWave = true
         // and reset that counter
 
-        if (inWave || timeSinceLastWaveEnd > 1000f)  
+        if (inWave)  
         {
-            InWave();
-            if (spawnManager.TotalEnemiesSpawned >= totalEnemiesInWave)
+            totalEnemiesSpawned = spawnManager.TotalEnemiesSpawned;
+            if (totalEnemiesSpawned >= totalEnemiesInWave)
             {
                 WaveIsStagnant();
+            }
+            else
+            {
+                InWave();
             }
         }
         else
         {
-            timeSinceLastWaveEnd += Time.unscaledDeltaTime;
+            if (timeSinceLastWaveEnd >= timeBetweenWaves)
+            {
+                StartWave(difficultyManager.Difficulty);
+            }
+            else
+            {
+                timeSinceLastWaveEnd += Time.unscaledDeltaTime;
+            }
         }
     }
 
 
-
-    // way to start a wave
-    // a wave needs to have either a amount of time or a number of enemies
-    //     probably a number of enemies
-
-    // need a way to determine that the wave has ended
-
+    void StartWave(float difficulty)
+    {
+        if (difficulty < 5)
+        {
+            totalEnemiesInWave = 10;
+        }
+        else
+        {
+            totalEnemiesInWave = 2 * (int)difficulty;
+        }
+        spawnManager.updateMaxSpawns();
+        inWave = true;
+        waveCount++;
+    }
     void InWave()
     {
-        inWave = true;
         spawnManager.continuouslySpawn();
-        timeSinceLastWaveEnd = 0;
-        //Debug.Log(spawnManager.numOfEnemiesSpawned());
-
+        
     }
 
+    // The enemies that have already been spawned are the last ones in the wave. No more spawning
     void WaveIsStagnant()
     {
-        inWave = true;
-        if (spawnManager.CurrNumOfEnemiesAlive() == 0) //&& timeSinceWaveStart < 10f)
+        if (spawnManager.CurrNumOfEnemiesAlive() == 0)
         {
             EndWave();
-
-            //Debug.Log("There are no enemies, end the wave");
         }
     }
     
     void EndWave()
     {
-        spawnManager.TotalEnemiesSpawned = 0;
+        spawnManager.TotalEnemiesSpawned = 0; // reset the spawn manager's count
         inWave = false;
     }
+
 }
