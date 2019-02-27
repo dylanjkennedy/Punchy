847d80a64e36ba154b202e49ea6d793b25352b30
Caleb Calvillo <calvillocaleb3@gmail.com>
2019-02-26 23:33:45 -0600

    Pickup finalization

--- Assets/Personal/Scripts/Pickup Scripts/HealthPickup.cs
+++ Assets/Personal/Scripts/Pickup Scripts/HealthPickup.cs
@@ -2,34 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealthPickup : MonoBehaviour
+public class HealthPickup : PickupController
 {
     [SerializeField] private int HealthAmount = 50;
 
-    // called once per frame
-
-    private void Update()
-    {
-        transform.Rotate(0, 1, 0);
-    }
 
     private void OnTriggerEnter(Collider col)
     {
         if (col.gameObject.tag == "Player")
         {
             col.gameObject.GetComponent<PlayerHealth>().GainHealth(HealthAmount);
-            col.gameObject.GetComponent<PickupSpawner>().PickedUp();
-            Destroy(gameObject);
+            col.gameObject.GetComponent<PickupSpawner>().PickedUp(gameObject);
+            //Destroy(gameObject);
         }
         if (col.gameObject.tag == "Enemy")
         {
             Physics.IgnoreCollision(col, gameObject.GetComponent<Collider>());
         }
     }
-
-    private void SpawnPickup(GameObject gameObject, GameObject tether)
-    {
-
-    }
-        
 }
---
+++ Assets/Personal/Scripts/Pickup Scripts/PickupController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PickupController : MonoBehaviour
+{
+    protected PickupSpawner.PickupType type;
+    public float timer;
+    protected float despawnRate = 20;
+
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        timer = 0;
+    }
+    
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        transform.Rotate(0, 100 * Time.deltaTime, 0);
+        timer += Time.deltaTime;
+        //print(timer);
+
+    }  
+
+
+}
