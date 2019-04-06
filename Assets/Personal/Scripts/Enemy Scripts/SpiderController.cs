cef04027c3088dc88dcc45cf73e18169e3f931fe
LueptowK <kylelueptow@gmail.com>
2019-04-06 16:27:35 -0500

    swarm initial commit

---
+++ Assets/Personal/Scripts/Enemy Scripts/SpiderController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderController : EnemyController
+{
+    float neighborRadius;
+    float cohesionWeight;
+    float separationWeight;
+    float goalWeight;
+    bool frozen;
+    Vector3 heading;
+    [SerializeField] SpiderSwarmController swarmController;
+    [SerializeField] float maxSpeed;
+    [SerializeField] Rigidbody rb;
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        if (!frozen)
+        {
+            rb.velocity = maxSpeed * heading;
+        }
+    }
+
+    public void SetHeading(Vector3 goal, Vector3 centerOfMass)
+    {
+        Vector3 goalVector = (goal - transform.position).normalized;
+        Collider[] neighbors = GetNeighbors();
+        Vector3 separationVector = Vector3.zero;
+        foreach(Collider neighbor in neighbors)
+        {
+            separationVector.x += transform.position.x - neighbor.transform.position.x;
+            separationVector.y += transform.position.y - neighbor.transform.position.y;
+            separationVector.z += transform.position.z - neighbor.transform.position.z;
+        }
+        Vector3.Normalize(separationVector);
+        Vector3 cohesionVector = goal - transform.position;
+        heading = Vector3.Normalize(separationVector * separationWeight + cohesionVector * cohesionWeight + goalVector * goalWeight);
+    }
+
+    public void SetController(SpiderSwarmController controller)
+    {
+        swarmController = controller;
+        neighborRadius = swarmController.neighborRadius;
+        cohesionWeight = swarmController.cohesionWeight;
+        separationWeight = swarmController.separationWeight;
+        goalWeight = swarmController.goalWeight;
+    }
+
+    private Collider[] GetNeighbors()
+    {
+        return Physics.OverlapSphere(transform.position, neighborRadius, LayerMask.GetMask("Spiders"));
+    }
+
+    public override void freeze()
+    {
+        rb.velocity = Vector3.zero;
+        frozen = true;
+    }
+}
---
+++ Assets/Personal/Scripts/Enemy Scripts/SpiderSwarmController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using System.Linq;
+
+public class SpiderSwarmController : MonoBehaviour
+{
+    public enum SwarmState : int { Chase = 0, Scatter, Regroup };
+    List<SpiderController> swarm;
+    Vector3 centerOfMass;
+    Vector3 CenterOfMass { get { return centerOfMass; } }
+    Vector3 averageHeading;
+    Vector3 AverageHeading { get { return averageHeading; } }
+    NavMeshPath path;
+    Vector3 goalPoint;
+    [SerializeField] float lengthOfScatter;
+    [SerializeField] float scatterLengthVariance;
+    [SerializeField] GameObject player;
+    float scatterTimer;
+    float scatterTimeout;
+
+    [SerializeField] public readonly float cohesionWeight;
+    [SerializeField] public readonly float separationWeight;
+    [SerializeField] public readonly float goalWeight;
+    [SerializeField] public readonly float neighborRadius;
+
+    SwarmState currentState;
+    public SwarmState CurrentState { get { return currentState; } }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        swarm = this.GetComponentsInChildren<SpiderController>().ToList();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (currentState)
+        {
+            case (SwarmState.Chase):
+                {
+                    centerOfMass = CalculateCenterOfMass();
+                    NavMesh.CalculatePath(centerOfMass, player.transform.position, NavMesh.AllAreas, path);
+                    foreach (SpiderController spider in swarm)
+                    {
+                        spider.SetHeading(path.corners[1], centerOfMass);
+                    }
+                    break;
+                }
+            case (SwarmState.Scatter):
+                {
+                    scatterTimer += Time.deltaTime;
+                    if (scatterTimer >= scatterTimeout)
+                    {
+                        currentState = SwarmState.Regroup;
+                    }
+                    break;
+                }
+            case (SwarmState.Regroup):
+                {
+                    //what does regroup detection look like? maybe calculate center of mass and check max distance from CoM?
+                    break;
+                }
+        }
+    }
+
+    public void Scatter()
+    {
+        currentState = SwarmState.Scatter;
+        scatterTimer = 0;
+        scatterTimeout = lengthOfScatter + Random.Range(-scatterLengthVariance, scatterLengthVariance);
+    }
+
+    private Vector3 CalculateCenterOfMass()
+    {
+        float x = 0f;
+        float y = 0f;
+        float z = 0f;
+        foreach(SpiderController spider in swarm)
+        {
+            x += spider.gameObject.transform.position.x;
+            y += spider.gameObject.transform.position.y;
+            z += spider.gameObject.transform.position.z;
+        }
+        return new Vector3(x / swarm.Count, y / swarm.Count, z / swarm.Count);
+    }
+}
